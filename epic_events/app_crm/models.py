b11ae0ed2b8387be19f0c09d0360bc346201e0be
AatroXiss <antoine.beaudesson@gmail.com>
2022-02-24 10:18:53 +0100

    v.0.0.4 added contract model

--- epic_events/app_crm/models.py
+++ epic_events/app_crm/models.py
@@ -1,6 +1,6 @@
 # models.py
 # created 22/02/2022 at 10:23 by Antoine 'AatroXiss' BEAUDESSON
-# last modified 24/02/2022 at 10:12 by Antoine 'AatroXiss' BEAUDESSON
+# last modified 24/02/2022 at 10:18 by Antoine 'AatroXiss' BEAUDESSON
 
 """ models.py
 
@@ -12,7 +12,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2020, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = "MIT"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -50,6 +50,7 @@ class Customer(models.Model):
     """
 
     customer_id = models.AutoField(primary_key=True)
+
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=254)
@@ -57,12 +58,33 @@ class Customer(models.Model):
     mobile = models.CharField(max_length=20)
     company_name = models.CharField(max_length=50)
     is_customer = models.BooleanField(default=False)
+
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
 
 class Contract(models.Model):
-    pass
+    """
+    This class represents a contract in the CRM.
+
+    Attributes:
+    :arg project_name: The name of the project.
+    :arg is_signed: A boolean indicating if the contract is signed.
+    :arg amount: The amount of the contract.
+    :arg payment_due_date: The payment due date of the contract.
+    """
+
+    contract_id = models.AutoField(primary_key=True)
+
+    project_name = models.CharField(max_length=100)
+    is_signed = models.BooleanField(default=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_due_date = models.DateField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    # Foreign keys
+    customer_id = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class Event(models.Model):
