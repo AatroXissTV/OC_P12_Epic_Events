53ecb867a2e67fbf3822a509fd045fd375703cef
AatroXiss <antoine.beaudesson@gmail.com>
2022-02-24 10:13:06 +0100

    v.0.0.3 added customer model

--- epic_events/app_crm/models.py
+++ epic_events/app_crm/models.py
@@ -1,6 +1,6 @@
 # models.py
 # created 22/02/2022 at 10:23 by Antoine 'AatroXiss' BEAUDESSON
-# last modified 22/02/2022 at 10:23 by Antoine 'AatroXiss' BEAUDESSON
+# last modified 24/02/2022 at 10:12 by Antoine 'AatroXiss' BEAUDESSON
 
 """ models.py
 
@@ -12,7 +12,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2020, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = "MIT"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -31,13 +31,39 @@ from django.db import models
 # constants
 
 
-class Event(models.Model):
-    pass
+class Customer(models.Model):
+    """
+    This class represents a customer in the CRM.
+    A prospect is immediately considered as a customer.
+    However, he truly becomes a customer when the field
+    'is_customer' is set to True.
+    By default the newly added person is a prospect (is_customer=False).
+
+    Attributes:
+    :arg first_name: The first name of the customer.
+    :arg last_name: The last name of the customer.
+    :arg email: The email of the customer.
+    :arg phone_number: The phone number of the customer.
+    :arg mobile: The mobile phone number of the customer.
+    :arg company_name: The company name of the customer.
+    :arg is_customer: A boolean indicating if the customer is a customer.
+    """
+
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
+    company_name = models.CharField(max_length=50)
+    is_customer = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
 
 class Contract(models.Model):
     pass
 
 
-class Customer(models.Model):
+class Event(models.Model):
     pass
