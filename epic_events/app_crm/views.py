83ae6247876d26e2447bb36ea236da1c81de916a
AatroXissTV <antoine.beaudesson@gmail.com>
2022-03-07 09:27:20 +0100

    v.0.0.15 Added basic views

--- epic_events/app_crm/views.py
+++ epic_events/app_crm/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+# app_crm/views.py
+# created 07/03/2022 at 09:22 by Antoine 'AatroXiss' BEAUDESSON
+# last modified 07/03/2022 at 09:22 by Antoine 'AatroXiss' BEAUDESSON
 
-# Create your views here.
+""" app_crm/views.py:
+    - *
+"""
+
+__author__ = "Antoine 'AatroXiss' BEAUDESSON"
+__copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
+__credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
+__license__ = ""
+__version__ = "0.0.15"
+__maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
+__email__ = "antoine.beaudesson@gmail.com"
+__status__ = "Development"
+
+# standard library imports
+
+# third party imports
+from rest_framework import viewsets
+
+# django imports
+
+# local application imports
+from .models import (
+    Customer,
+    Contract,
+    Event
+)
+from .serializers import (
+    CustomerSerializer,
+    ContractSerializer,
+    EventSerializer
+)
+
+# other imports & constants
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows customer to be created or viewed.
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ContractViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows contract to be created or viewed.
+    queryset = Contract.objects.all()
+    serializer_class = ContractSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows event to be created or viewed.
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
