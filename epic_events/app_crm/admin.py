d6d246e5648339556d4067c2885d6a40a30697bf
AatroXissTV <antoine.beaudesson@gmail.com>
2022-03-08 09:25:48 +0100

    v.0.0.20 added admin.register for customer

--- epic_events/app_crm/admin.py
+++ epic_events/app_crm/admin.py
@@ -10,7 +10,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = ""
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -34,7 +34,33 @@ from .models import (
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (
+            "Customer/Prospect informations",
+            {"fields": ("first_name", "last_name", "email",
+                        "phone_number", "mobile", "company_name",
+                        "is_customer")}
+        )
+        (
+            "Sales contact informations",
+            {"fields": ("sales_contact_id",)}
+        )
+        (
+            "Date informations",
+            {"fields": ("date_created", "date_updated")}
+        )
+    )
+    read_only_fields = ('date_created', 'date_updated')
+    list_display = ('first_name', 'last_name', 'email',
+                    'phone_number', 'mobile', 'company_name',
+                    'is_customer',)
+    list_filter = ('is_customer', 'sale_contact_id',)
+    search_fields = ('first_name', 'last_name', 'company_name',
+                     'sales_contact_id')
+
+    @staticmethod
+    def full_name(obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 @admin.register(Contract)
