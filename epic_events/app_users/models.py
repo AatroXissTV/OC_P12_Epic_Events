affbb85e7fa0aaa7934bb2b8256e22201c2f2726
AatroXissTV <antoine.beaudesson@gmail.com>
2022-03-08 10:59:02 +0100

    v.0.1.0 Admin is done

--- epic_events/app_users/models.py
+++ epic_events/app_users/models.py
@@ -10,7 +10,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = ""
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -46,3 +46,30 @@ class User(AbstractUser):
     date_updated = models.DateTimeField(auto_now=True)
 
     # FKs
+
+    # Methods
+    def __str__(self):
+        """
+        This method returns the string representation of the object.
+        """
+        return f"{self.username} ({self.role})"
+
+    def save(self, *args, **kwargs):
+        """
+        This method saves the object.
+        """
+        if self.role == 'management':
+            self.is_staff = True
+            self.is_superuser = True
+        else:
+            self.is_staff = False
+            self.is_superuser = False
+
+        user = super(User, self)
+
+        if len(self.password) != 88:
+            user.set_password(self.password)
+
+        user.save()
+
+        return user
