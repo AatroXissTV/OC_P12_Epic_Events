6e7ee2b8cbf27f04a843868de869e92d1855851e
AatroXissTV <antoine.beaudesson@gmail.com>
2022-04-05 12:08:02 +0200

    v.0.2.7 updates on event perform create

--- epic_events/app_crm/views.py
+++ epic_events/app_crm/views.py
@@ -1,6 +1,6 @@
 # app_crm/views.py
 # created 07/03/2022 at 09:22 by Antoine 'AatroXiss' BEAUDESSON
-# last modified 30/03/2022 at 12:50 by Antoine 'AatroXiss' BEAUDESSON
+# last modified 05/04/2022 at 12:07 by Antoine 'AatroXiss' BEAUDESSON
 
 """ app_crm/views.py:
     - *
@@ -10,7 +10,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = ""
-__version__ = "0.2.3"
+__version__ = "0.2.6"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -148,8 +148,8 @@ class EventViewSet(ModelViewSet):
             raise PermissionDenied('You cannot create an event with a contract not signed')  # noqa
         if Event.objects.filter(contract_id=serializer.validated_data.get('contract_id')).exists():  # noqa
             raise PermissionDenied('You cannot create an event for a contract that already has an event')  # noqa
-        if not Contract.objects.filter(id=serializer.validated_data.get('contract_id')).exists():  # noqa
-            raise PermissionDenied("You can't create an event for a contract that does not exist")  # noqa
+        if serializer.validated_data.get('contract_id').id is None:
+            raise PermissionDenied('You cannot create an event for a contract that does not exist')  # noqa
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
